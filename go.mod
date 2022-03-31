3e8233804dca9ec6c0aaedecd09755dbf42127b8
alexpfx <alexpfba@gmail.com>
2022-03-31 19:48:50 -0300

    change folder

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/alexpfx/gofi
 
 go 1.18
-
-require golang.org/x/tools v0.1.8-0.20211029000441-d6a9af8af023
-
-require golang.org/x/mod v0.6.0-dev.0.20211013180041-c96bc1413d57 // indirect
--- rofi.go
+++ rofi.go
@@ -1,36 +1,25 @@
-package rofi
+package gofi
 
 import (
 	"fmt"
-	"log"
 	"os/exec"
 )
 
 const rofi = "rofi"
-
-func checkRofi() bool {
-	cmd := exec.Command("command", "-v", rofi)
-	if err := cmd.Run(); err != nil {
-		fmt.Println(err)
-		return false
-	}
-	return true
-}
 
 func CallRofi(input string, args []string) (string, error) {
 	fmt.Println(input)
 	fmt.Println(args)
 	cmd := exec.Command(rofi, args...)
 	in, err := cmd.StdinPipe()
-	if err != nil{
-		log.Fatal(err)
+	if err != nil {
+		return "", err
 	}
-	
-	go func ()  {
+
+	go func() {
 		fmt.Fprintln(in, input)
 		defer in.Close()
 	}()
-
 
 	res, err := cmd.Output()
 	return string(res), err
